54275d641ee0bcd0e5e3de78673acdd07478cd8f
Denzil <denzil.dbn@gmail.com>
2025-09-16 13:26:24 +0200

    Setup for UI Test

--- Automation_Assessment.UI/UnitTest1.cs
+++ Automation_Assessment.UI/UnitTest1.cs
@@ -1,15 +1,36 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
 namespace Automation_Assessment.UI
 {
+    [TestFixture]
     public class Tests
     {
+        private IBrowser _browser;
+        private IPage _page;
+        private IBrowserContext _context;
+
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
         }
 
         [Test]
-        public void Test1()
+        public async Task Given_When_Then()
         {
+            // Arrange
+
+            // Act
+
+            // Assert
             Assert.Pass();
         }
     }
